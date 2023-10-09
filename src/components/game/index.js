1e2ac47bbe74247e84e447e59d154b6d8af2a5d1
IoanAugustin98 <simuleac.ioan@yahoo.com>
2023-10-09 19:29:29 +0300

    Add lodash.shuffle

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -1,1 +1,18 @@
 export * from './Game'
+
+const difficulty = {
+    easy: 12
+};
+
+export const icond = [
+    'fa-solid fa-house',
+    'fa-solid fa-magnifying-glass',
+    'fa-solid fa-user',
+    'fa-solid fa-check',
+    'fa-solid fa-download',
+    'fa-solid fa-image'
+];
+
+export const generateTilesArray = ( difficulty = 'easy' ) => {
+
+};
