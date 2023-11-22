50f2dec450c94fc8ed562b997dcfafc84a154d16
IoanAugustin98 <simuleac.ioan@yahoo.com>
2023-11-22 19:16:54 +0200

    Some CSS modifications

--- src/components/game/GameControls.jsx
+++ src/components/game/GameControls.jsx
@@ -5,7 +5,7 @@ import { GameTimer } from './GameTimer';
 export const GameControls = () => {
     const { dispatch, state } = useContext(gameContext);
     const { timeLeft } = state;
-    const buttonClasses = 'text-2xl inline-flex border border-light px-16 py-2 bg-gray-300 rounded-lg';
+    const buttonClasses = 'text-2xl inline-block border border-light py-2 bg-gray-300 rounded-lg basis-1/3 text-center';
   return (
     <div className='flex justify-between'>
         <button 
--- src/components/game/GameTimer.jsx
+++ src/components/game/GameTimer.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const p = ( number, maxLength = 2, padString = '0' ) => {
     return number.toString().padStart(maxLength,padString);
 };
-const buttonClasses = 'text-2xl inline-flex border border-light px-16 py-2 bg-gray-300 rounded-lg';
+const buttonClasses = 'text-2xl inline-block border border-light py-2 bg-gray-300 rounded-lg basis-1/3 text-center';
 
 export const GameTimer = ({ timeLeft }) => {
     const renderTime = () => {
         const t = new Date(timeLeft);
-        return `${p(t.getMinutes())}:${p(t.getSeconds())}`;
+        return <span className='font-mono'>{p(t.getMinutes())}:{p(t.getSeconds())}</span>;
     };
     return (
         <div className={buttonClasses}>
-            <span>Time {renderTime()}</span>
+            <span>Time: {renderTime()}</span>
         </div>
     );
 };
